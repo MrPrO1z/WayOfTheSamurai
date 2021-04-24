65928aba8718f39c2bfb2f546c73f587821d160a
vitaliy <artemenko1986@gmail.com>
2021-04-24 04:11:10 +0300

    путь самурая 001

---
+++ src/technologies.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>javascript</li>
+                <li>HTML</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
+}
+export default Technologies;
